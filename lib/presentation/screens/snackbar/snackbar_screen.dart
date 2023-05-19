127c6bfaa785e2bb55197e3f0284ffb5d076155d
jaimetellezb <jaimetellez410@gmail.com>
2023-05-19 12:03:28 -0500

    snackbars impl

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,32 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar(BuildContext context) {
+    // ocultar el snackbar anterior
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final SnackBar snackBar = SnackBar(
+      content: const Text('Hola Mundo'),
+      // botón que hace x acción
+      action: SnackBarAction(label: 'Ok', onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    );
+
+    // mostrar snackbar
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y díalogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
